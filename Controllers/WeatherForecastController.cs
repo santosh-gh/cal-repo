0f189bc4bbaca9c7c99be839d3d6b787847c9b20
unknown <ENCTEST@SAGE-ENC-L120.isgn.com>
2021-04-13 21:01:45 +0530

    modified

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,8 +29,8 @@ namespace Calculation.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                DATE = DateTime.Now.AddDays(index),
-                TEMP = rng.Next(-20, 55),
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
